69030be1b8d75b2ffd038fa5519c77e35f6cd331
Richard Rubio <rrubiorr81@gmail.com>
2015-08-28 19:24:40 -0400

    adding stop button, and some minor effects

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
 
+        playButton.enabled = false;
+        recordingInProgress.hidden = false;
+        stopButton.hidden = false;
+        //TODO: record user's voice
+        println("grabando voz");
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //hide stop button
+        stopButton.hidden = true;
+    }
+
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        recordingInProgress.hidden = true;
+        playButton.enabled = true;
+    }
+    
 }
 
